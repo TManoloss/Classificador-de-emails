dca11bbb216e60084b81dbaae32b7e7b5b2158ba
Manoel Carvalho <manoelfelipe90@gmail.com>
2025-09-16 16:00:13 -0300

    fix: error url render

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -12,7 +12,7 @@ const RENDER_BACKEND = "https://classificador-de-emails-hxkx.onrender.com";
 const apiBaseUrl = isProduction ? RENDER_BACKEND : "http://127.0.0.1:8000";
 
 export const CONFIG = {
-  API_URL: `${apiBaseUrl}/classify`, // certifique-se que seu backend usa /classify
+  API_URL: `${apiBaseUrl}/api/classify`, // certifique-se que seu backend usa /classify
   MAX_FILE_SIZE: 10 * 1024 * 1024,
   MAX_CHARS: 5000,
   TOAST_DURATION: 3000,
